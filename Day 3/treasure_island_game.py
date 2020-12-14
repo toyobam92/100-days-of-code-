b247af583ce881b17f83ab30613ec803b95d12e0
toyobam92 <56419744+toyobam92@users.noreply.github.com>
2020-12-14 16:31:39 -0600

    Create treasure_island_game.py

---
+++ Day 3/treasure_island_game.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+direction = input(
+    "You enter a room, it's full of zoombies, What direction will you like to go? type left or right?\n"
+).lower()
+if direction =="left":
+    activity = input(
+        "You have two choices,are afraid of water or heights ,choose swim or climb ?\n"
+    ).lower()
+    if activity == "climb":
+        door = input(
+            "You are on the 30th floor, There are 3 doors, Which door will you like to enter type red, blue or yellow\n"
+        ).lower()
+        if door == "yellow":
+            print("You escaped!!!, You win!")
+        elif door == "red":
+            print("Aww too bad!! Game over")
+        elif door == "blue":
+            print("This is the Zoombies den ;(! Game Over")
+        else:
+            print("You chose a door that doesnt exist, Game over ")
+    else:
+        print('Game Over')
+else:
+    print('Game Over')
