f5c3e721f1f9b0bad4776295645e777222fd07a8
toyobam92 <56419744+toyobam92@users.noreply.github.com>
2020-12-16 12:48:45 -0600

    Create highest_score_check.py

---
+++ highest_score_check.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+y = 0
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+      highest_score = score
+print(f"The highest score in the class is {highest_score}")
+
+  
+
