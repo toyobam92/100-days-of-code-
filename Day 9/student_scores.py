46ae6c117d9fc1a8902504b292ba5bc1285c55d7
Toyosi <56419744+toyobam92@users.noreply.github.com>
2020-12-20 08:32:02 -0600

    Update student_scores.py

--- Day 9/student_scores.py
+++ Day 9/student_scores.py
@@ -1,6 +1,6 @@
 student_scores = {
   "Harry": 81,
-  "Ron": 78,
+  "Ron": 70,
   "Hermione": 99, 
   "Draco": 74,
   "Neville": 62,
@@ -13,13 +13,13 @@ student_grades = {}
 #TODO-2: Write your code below to add the grades to student_grades.👇
 for key in student_scores:
   grade_num = student_scores[key]
-  if grade_num > 91 and grade_num < 100:
+  if grade_num > 90:
     student_grades[key] = "Outstanding"
-  elif grade_num > 81 and grade_num < 90:
+  elif grade_num > 80:
     student_grades[key] ="Exceeds Expectations"
-  elif grade_num > 71 and grade_num < 80:
+  elif grade_num > 70:
     student_grades[key] = "Acceptable"
-  elif grade_num < 70:
+  else:
     student_grades[key] = "Fail"
 # 🚨 Don't change the code below 👇
 print(student_grades)
