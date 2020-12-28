d8c03f1a0fb37a8b3d71b194a6af7c434e212aad
Toyosi <56419744+toyobam92@users.noreply.github.com>
2020-12-28 09:52:13 -0600

    Create Classes Coffee Maker.py

---
+++ Day 16/Classes Coffee Maker.py
@@ -0,0 +1,28 @@
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+menus = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+
+while is_on:
+    options = menus.get_items()
+    choice = input(f"What would you like ?({options}):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menus.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
+            #make payment executes process coins()
+            #money_machine.make_payment(drink.cost)
