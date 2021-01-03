fd8e7f8382147c3320195a9d8bd2aba1fd62c604
Toyosi <56419744+toyobam92@users.noreply.github.com>
2021-01-03 11:00:34 -0600

    Create higher_order_func.py

---
+++ Day 19/higher_order_func.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+#
+# def move_forwards():
+#     tim.forward(10)
+#
+#
+# screen.listen()
+# screen.onkey(key="space", fun=move_forwards)
+# screen.exitonclick()
+
+#HIGH ORDER FUNCTIONS - FUNCTIONS THAT CAN BE USED IN ORDER FUNCTIONS
+
+
+def move_forwards():
+    tim.forward(10)
